4f1dab87184b329eaa0e90de783c6d33302816d9
Arthus Lucic <arthusrdz@gmail.com>
2026-03-02 12:15:15 +0100

    ivec2_get/setmap

---
+++ ivec2.cpp
@@ -0,0 +1,107 @@
+
+#include "ivec2.hpp"
+
+#include <cmath>
+#include <cassert>
+#include <iostream>
+
+
+
+
+ivec2::ivec2()
+    :x(0.0f),y(0.0f)
+{}
+
+ivec2::ivec2(int const x_param,int const y_param)
+    :x(x_param),y(y_param)
+{}
+
+ivec2& operator+=(ivec2& lhs,ivec2 const& rhs)
+{
+    lhs.x+=rhs.x;
+    lhs.y+=rhs.y;
+    return lhs;
+}
+
+ivec2& operator-=(ivec2& lhs,const ivec2& rhs)
+{
+    lhs.x-=rhs.x;
+    lhs.y-=rhs.y;
+    return lhs;
+}
+
+ivec2& operator*=(ivec2& v,float const s)
+{
+    v.x*=s; v.y*=s;
+    return v;
+}
+
+ivec2& operator/=(ivec2& v,float const s)
+{
+    assert(fabs(s)>10e-6);
+    v.x/=s; v.y/=s;
+    return v;
+}ivec2& operator*=(ivec2& v,int const s)
+{
+    v.x*=s; v.y*=s;
+    return v;
+}
+
+ivec2& operator/=(ivec2& v,int const s)
+{
+    assert(fabs(s)>10e-6);
+    v.x/=s; v.y/=s;
+    return v;
+}
+
+std::ostream& operator<<(std::ostream& sout,ivec2 const& v)
+{
+    sout<<"["<<v.x<<";"<<v.y<<"]";
+    return sout;
+}
+
+
+int dot(ivec2 const& v0,ivec2 const& v1)
+{
+    return v0.x*v1.x+v0.y*v1.y;
+}
+
+
+
+ivec2 operator+(ivec2 const& v0,ivec2 const& v1)
+{
+    ivec2 temp=v0;
+    temp+=v1;
+    return temp;
+}
+
+ivec2 operator-(ivec2 const& v0,ivec2 const& v1)
+{
+    ivec2 temp=v0;
+    temp-=v1;
+    return temp;
+}
+
+ivec2 operator*(ivec2 const& v0,float const s)
+{
+    ivec2 temp=v0;
+    temp*=s;
+    return temp;
+}
+
+ivec2 operator*(float s,ivec2 const& v0)
+{
+    return v0*s;
+}
+ivec2 operator*(int s,ivec2 const& v0)
+{
+    return v0*s;
+}
+
+ivec2 operator/(ivec2 const& v0,float const s)
+{
+    ivec2 temp=v0;temp/=s;
+    return temp;
+}
+
+
---
+++ ivec2.hpp
@@ -0,0 +1,64 @@
+
+#pragma once
+
+#ifndef IVEC2_HPP
+
+#define IVEC2_HPP
+
+#include <iostream>
+
+
+/** A 2D vector */
+struct ivec2
+{
+    /** x coordinate */
+    int x;
+    /** y coordinate */
+    int y;
+
+    /** Constructor vec (0,0) */
+    ivec2();
+    /** Constructor vec (x,y) */
+    ivec2(int x_param,int y_param);
+
+};
+
+
+
+/** Scalar product */
+int dot(ivec2 const& v0,ivec2 const& v1);
+
+/** Return a vector of same direction with unit length */
+
+
+/** Print the content of a vector on the command line */
+std::ostream& operator<<(std::ostream& sout,ivec2 const& m);
+
+/** Vectorial sum */
+ivec2& operator+=(ivec2& lhs,ivec2 const& rhs);
+/** Vectorial difference */
+ivec2& operator-=(ivec2& lhs,ivec2 const& rhs);
+/** Multiplication by a scalar */
+ivec2& operator*=(ivec2& v,float s);
+/** Division by a scalar */
+ivec2& operator/=(ivec2& v,float s);
+
+ivec2& operator*=(ivec2& v,int s);
+/** Division by a scalar */
+ivec2& operator/=(ivec2& v,int s);
+
+/** Vectorial sum */
+ivec2 operator+(ivec2 const& v0,ivec2 const& v1);
+/** Vectorial difference */
+ivec2 operator-(ivec2 const& v0,ivec2 const& v1);
+/** Multiplication with a scalar */
+ivec2 operator*(ivec2 const& v0,float s);
+/** Multiplication with a scalar (left) */
+ivec2 operator*(float s,ivec2 const& v0);
+/** Division by a scalar */
+ivec2 operator/(ivec2 const& v0,float s);
+
+
+
+
+#endif
