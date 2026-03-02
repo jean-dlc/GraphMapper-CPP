79193b60556a3d72fda4f88bcfd9505178c208b7
Arthus Lucic <arthusrdz@gmail.com>
2026-03-02 09:56:32 +0100

    crea_class_map

---
+++ map.cpp
@@ -0,0 +1,11 @@
+std::vector<vec2> Grille;
+float x,y;
+Grille.reserve(400);  //lancement du jeu permet de gagner en efficacité sans avoir a réalouer la grille
+for(int i=0;i<400;i++){
+    x= i%20
+    y= i/20
+    i= new case((x,y),bool 1,bool 0)
+    
+
+
+}
